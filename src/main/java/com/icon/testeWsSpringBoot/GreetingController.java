4d03ae5df0c16ec4148e4016c97943cbc87fda67
rayfran.rocha <rayfran.rocha.lima@gmail.com>
2020-03-27 01:20:07 -0400

    primeira versao com rest ok

---
+++ src/main/java/com/icon/testeWsSpringBoot/GreetingController.java
@@ -0,0 +1,21 @@
+package com.icon.testeWsSpringBoot;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+	
+	private static final String template = "Hello, %s!";
+	
+	private static final AtomicLong counter = new AtomicLong();
+	
+	@RequestMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "nome", defaultValue = "World") String nome) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, nome));
+	}
+
+}
---
+++ src/test/java/com/icon/testeWsSpringBoot/TesteWsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icon.testeWsSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteWsSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
